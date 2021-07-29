4ee6608cce5ade0f9a6cc815712ca291f098d0b4
Jadson Santos <jadson44.santos@gmail.com>
2021-07-29 20:12:09 -0300

    core: Adicionado arquivos sql

---
+++ src/database/seeds.sql
@@ -0,0 +1,60 @@
+
+## User
+
+INSERT INTO users VALUES (NULL,"Jadson dos Santos Silva","jadson44.santos@gmail.com","15859eb4-f0bb-11eb-aef4-9b77f48ed9b1","15859eb4-f0bb-11eb-aef4-9b77f48ed9b1","http://0.0.0.0:1993/user/0000000001.png","");
+INSERT INTO users VALUES (NULL,"Bruna Farias","bruna.farias@gmail.com","110e5682-f0bb-11eb-9ae0-6b6cc40c834a","110e5682-f0bb-11eb-9ae0-6b6cc40c834a","http://0.0.0.0:1993/user/0000000002.png","");
+INSERT INTO users VALUES (NULL,"Jeferson Silva","jeferson.silva@gmail.com","d87bd4d4-f0ba-11eb-88c0-93c01c1a6c5c","d87bd4d4-f0ba-11eb-88c0-93c01c1a6c5c","http://0.0.0.0:1993/user/0000000003.png","");
+INSERT INTO users VALUES (NULL,"Ana Maria","ana.maria@gmail.com","e63cf972-f0ba-11eb-9b2a-d37ad93d7040","e63cf972-f0ba-11eb-9b2a-d37ad93d7040","http://0.0.0.0:1993/user/0000000004.png","");
+INSERT INTO users VALUES (NULL,"Juliana Farias","juliana.farias@gmail.com","f1094eb4-f0ba-11eb-9f82-f76d965cfa58","f1094eb4-f0ba-11eb-9f82-f76d965cfa58","http://0.0.0.0:1993/user/0000000005.png","");
+INSERT INTO users VALUES (NULL,"Gustavo Nascimento","gustavo.nascimento@gmail.com","f587855a-f0ba-11eb-89c1-c747f7633506","f587855a-f0ba-11eb-89c1-c747f7633506","http://0.0.0.0:1993/user/0000000006.png","");
+INSERT INTO users VALUES (NULL,"Teston nascimento","teston.nascimento@gmail.com","fa684776-f0ba-11eb-a557-bba96bc0a5e7","fa684776-f0ba-11eb-a557-bba96bc0a5e7","http://0.0.0.0:1993/user/0000000007.png","");
+INSERT INTO users VALUES (NULL,"Lucineia","lucineia@gmail.com","03cf2d66-f0bb-11eb-ae7c-b34663324ce0","03cf2d66-f0bb-11eb-ae7c-b34663324ce0","http://0.0.0.0:1993/user/0000000008.png","");
+
+
+## Elos
+
+INSERT INTO elos VALUES (NULL,"LALALALALALALALLALALALALALLALALALALLALALALA :)",0,0,"muito barato","rua da trindade, 394, Agronômica, Brasil",1);
+INSERT INTO elos VALUES (NULL,"LALALALALALALALLALALALALALLALALALALLALALALA :)",0,0,"barato","rua dos pecados, 131, Bahia, Brasil",2);
+INSERT INTO elos VALUES (NULL,"LALALALALALALALLALALALALALLALALALALLALALALA :)",0,0,"caro","rua das fadas,942, Manaus, Brasil",3);
+INSERT INTO elos VALUES (NULL,"LALALALALALALALLALALALALALLALALALALLALALALA :)",0,0,"muito caro","rua das justificativas, 171, Distrito federal, Brasil",4);
+
+## Comments
+
+INSERT INTO comments VALUES (NULL,"Muito massaaaaaaaaa",1,1);
+INSERT INTO comments VALUES (NULL,"Lindooo",2,2);
+INSERT INTO comments VALUES (NULL,"Minha nossaaaa",3,3);
+INSERT INTO comments VALUES (NULL,"Legal",1,1);
+INSERT INTO comments VALUES (NULL,"massaaaaaaaaa cara",1,1);
+
+
+## Likes 
+
+INSERT INTO likes VALUES (NULL,1,1);
+INSERT INTO likes VALUES (NULL,1,2);
+INSERT INTO likes VALUES (NULL,1,3);
+INSERT INTO likes VALUES (NULL,2,1);
+INSERT INTO likes VALUES (NULL,2,2);
+INSERT INTO likes VALUES (NULL,2,3);
+
+
+## Videos 
+
+INSERT INTO videos VALUES (NULL,"http://0.0.0.0:1993/video/747e5102-f0bd-11eb-9aa6-ef5ef7a84350.mp4",1);
+INSERT INTO videos VALUES (NULL,"http://0.0.0.0:1993/video/747ec112-f0bd-31eb-9aa6-ef5ef7a84350.mp4",2);
+INSERT INTO videos VALUES (NULL,"http://0.0.0.0:1993/video/747e112-f0bd-41eb-9aa6-ef5ef7a8f350.mp4",1);
+INSERT INTO videos VALUES (NULL,"http://0.0.0.0:1993/video/747e51a2-f0bd-51eb-faa6-ef5ef7a84350.mp4",3);
+INSERT INTO videos VALUES (NULL,"http://0.0.0.0:1993/video/747ec5102-f0bd-51eb-faa6-ef5ef7a84350.mp4",3);
+INSERT INTO videos VALUES (NULL,"http://0.0.0.0:1993/video/747ec102-f0bd-51eb-faa6-ef5ef7a84350.mp4",2);
+INSERT INTO videos VALUES (NULL,"http://0.0.0.0:1993/video/747ec102-f0bd-51eb-aaa6-ef5ef7a84350.mp4",2);
+
+
+## Images
+
+
+INSERT INTO images VALUES (NULL,"http://0.0.0.0:1993/video/747e5102-f0bd-11eb-9aa6-ef5ef7a84351.png",1);
+INSERT INTO images VALUES (NULL,"http://0.0.0.0:1993/video/747ec112-f0bd-31eb-9aa6-ef5ef7a84352.png",2);
+INSERT INTO images VALUES (NULL,"http://0.0.0.0:1993/video/747e112-f0bd-41eb-9aa6-ef5ef7a8f353.png",1);
+INSERT INTO images VALUES (NULL,"http://0.0.0.0:1993/video/747e51a2-f0bd-51eb-faa6-ef5ef7a84354.png",3);
+INSERT INTO images VALUES (NULL,"http://0.0.0.0:1993/video/747ec5102-f0bd-51eb-faa6-ef5ef7a84355.png",3);
+INSERT INTO images VALUES (NULL,"http://0.0.0.0:1993/video/747ec102-f0bd-51eb-faa6-ef5ef7a84356.png",2);
+INSERT INTO images VALUES (NULL,"http://0.0.0.0:1993/video/747ec102-f0bd-51eb-aaa6-ef5ef7a84357.png",2);
