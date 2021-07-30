a64cbebaa2efa31aab025706fb8d6e938f81316b
Jadson Santos <jadson44.santos@gmail.com>
2021-07-30 19:50:24 -0300

    core: Criado caso de teste para solicitar o id quando um usuario for atualizado

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -2,16 +2,25 @@
 import connection from '../database/connection.ts'
 import User from '../model/user.model.ts'
 
+
 import { 
     MESSAGE_SUCESS_CREATE_USER,
     MESSAGE_USER_ALREADY_EXISTS_EMAIL,
     MESSAGE_USER_ALREADY_EXISTS_USERNAME,
     MESSAGE_INTERNAL_SERVER_ERROR,
-MESSAGE_SUCCESS_UPDATE_USER
+    MESSAGE_SUCCESS_UPDATE_USER,
+    SCHEMEA_UPDATE_USER,
+SCHEMEA_CREATE_USER
 } from "../const.ts";
 
 export async function create_user(user:User) {
    try {
+
+    const errors = SCHEMEA_CREATE_USER.validate(user as any)
+    
+    if (errors.length > 0) {
+        return { error: true, message: errors[0].message, status: 400  }
+    }
 
     await connection.execute(`INSERT INTO users VALUES (NULL,?,?,?,?,?,?);`,[
         user.name,
@@ -45,8 +54,13 @@ export async function create_user(user:User) {
 
 export async function update_user(user:User) {
     try {
- 
-
+    
+    const errors = SCHEMEA_UPDATE_USER.validate(user as any)
+    
+    if (errors.length > 0) {
+        return { error: true, message: errors[0].message, status: 400  }
+    }
+    
     await connection.execute(`UPDATE users SET name = ?, email = ?, username = ?, password = ?, photo = ?, bio = ? WHERE id = ?;`,[
          user.name,
          user.email,
--- test/users.test.ts
+++ test/users.test.ts
@@ -1,15 +1,11 @@
-import { assertEquals } from "https://deno.land/std@0.103.0/testing/asserts.ts";
+// deno-lint-ignore-file
+import { assertEquals,assertStrictEquals} from "https://deno.land/std@0.103.0/testing/asserts.ts";
 
 import { create_user, update_user } from '../src/service/user.service.ts'
 
-import {
-    MESSAGE_SUCESS_CREATE_USER,
-    MESSAGE_USER_ALREADY_EXISTS_EMAIL,
-    MESSAGE_USER_ALREADY_EXISTS_USERNAME
-} from '../src/const.ts'
 
 
-Deno.test("[service] Deveria criar um usuario", async () => {
+Deno.test("Deveria criar um usuario", async () => {
     
     const {error } = await create_user({
         name: "Jadson dos Santos Silva",
@@ -28,7 +24,7 @@ Deno.test("[service] Deveria criar um usuario", async () => {
 
 });
 
-Deno.test("[service] Deveria atualizar um usuario", async () => {
+Deno.test("Deveria atualizar um usuario", async () => {
     
     const { error } = await update_user({
         name: "Jadson dos Santos Silva",
@@ -47,5 +43,21 @@ Deno.test("[service] Deveria atualizar um usuario", async () => {
         assertEquals<boolean>(error,false)
     }
 
+});
+
+Deno.test("Deveria solicitar o id quando atualizar o usuário", async () => {
+    
+    const { error,message } = await update_user({
+        name: "Jadson dos Santos Silva",
+        email: "jadson444.santos@gmail.com",
+        username: "15859eb4-f0bb-11eb-aef4-9b77f48ed9b1",
+        password: "15859eb4-f0bb-11eb-aef4-9b77f48ed9b1",
+        photo: "http://0.0.0.0:1993/user/0000000001.png",
+        bio: "",
+        //id: 1
+    })
+
+    assertStrictEquals(message,"id é obrigatório")
+    assertEquals<boolean>(error,true)
 });
 
