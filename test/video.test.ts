c81a642845f883381c44e5952db172b39d24c78c
Jadson Santos <jadson44.santos@gmail.com>
2021-08-05 16:55:14 -0300

    core: Criados testes para busca de video e images vinculadas aos elos

--- test/video.test.ts
+++ test/video.test.ts
@@ -2,7 +2,8 @@
 import { assertEquals} from "https://deno.land/std@0.103.0/testing/asserts.ts";
 
 import {
-    create_video
+    create_video,
+    search_videos
 } from '../src/service/video.service.ts'
 
 Deno.test("Deveria adicionar um video no elo 4", async () => {
@@ -12,6 +13,15 @@ Deno.test("Deveria adicionar um video no elo 4", async () => {
         id_elo: 5
     })
     
+    assertEquals<boolean>(error,false)
+
+});
+
+Deno.test("Deveria obter todas as videos vinculados ao elo 4", async () => {
+    
+    const { error } = await search_videos({
+        id_elo: 2
+    })
     assertEquals<boolean>(error,false)
 
 });
