afc7b0262d4112135160339c27b317e69574d1df
Jadson Santos <jadson44.santos@gmail.com>
2021-10-06 18:46:49 -0300

    core: Melhorias nos testes

--- test/comments.test.ts
+++ test/comments.test.ts
@@ -7,25 +7,49 @@ import {
     search_comments,
     update_comment,
 } from "../src/service/comment.service.ts"
+import { search_elos } from "../src/service/elo.service.ts";
+
+
+Deno.test("Deveria criar comentários em um elo", async () => {
+
+    const { elos } = await search_elos(
+        {
+        address: "%Almirante%",
+        qtd_comments: 0,
+        qtd_likes: 0,
+        category: "caro",
+        description: "%%",
+    },0)
+
+
+    const {error,status} = await create_comment({
+        content: "Meu deus <3",
+        id_elo: elos[0].id,
+        id_user: 2,
+    })
+    assertEquals(error,false)
+    assertEquals(status,200)
+})
 
 Deno.test("Deveria procurar por comentários de um elo", async () => {
+
+    const { elos } = await search_elos(
+        {
+        address: "%Almirante%",
+        qtd_comments: 0,
+        qtd_likes: 0,
+        category: "caro",
+        description: "%%",
+    },0)
+
+
     const {comments,error,status} = await search_comments({
-        id_elo: 2,
+        id_elo: elos[0].id,
     },0)
 
     assertEquals(error,false)
     assertEquals(status,200)
     assert(comments.length > 0)
-})
-Deno.test("Deveria criar comentários em um elo", async () => {
-
-    const {error,status,message} = await create_comment({
-        content: "Meu deus <3",
-        id_elo: 2,
-        id_user: 2,
-    })
-    assertEquals(error,false)
-    assertEquals(status,200)
 })
 
 Deno.test("Deveria atualizar comentário de um elo", async () => {
--- test/likes.test.ts
+++ test/likes.test.ts
@@ -5,11 +5,22 @@ import {
     register_like_in_elo,
     find_like_in_elo
 } from '../src/service/like.service.ts'
+import { search_elos } from "../src/service/elo.service.ts";
 
-Deno.test("Deveria registrar um like no elo 2", async () => {
+Deno.test("Deveria registrar um like no elo ", async () => {
+
+    const { elos } = await search_elos(
+        {
+        address: "%Almirante%",
+        qtd_comments: 0,
+        qtd_likes: 0,
+        category: "caro",
+        description: "%%",
+    },0)
+
     const { error,message,status } = await register_like_in_elo({
         id_user: 2,
-        id_elo: 2
+        id_elo: elos[0].id
     })
     assertEquals(status,200)
     assertEquals<boolean>(error,false)
